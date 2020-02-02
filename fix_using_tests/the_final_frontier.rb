580b7c7ba6de62419b22295316edefe07a3a9300
lior Shine <shinelior@gmail.com>
2020-02-02 00:04:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -13,12 +13,13 @@ def crew
 end
 
 def greet_crew (crew)
-  binding.pry
+ 
   crew.each {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
   date = generate_star_date
+  
   state_log(date)
   greet_crew(crew)
 end
